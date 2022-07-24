e223c350c9db37065693861574673e55b6d522c3
Caleb Standridge <99684824+CStan18@users.noreply.github.com>
2022-07-24 15:25:16 -0500

    Print matrix: added Compare vectors: next

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -10,4 +10,32 @@ int main()
 	// Rows and columns 0-2
 	printBoard();
 
+	// printBoard() could maybe go at the beginning of every turn
+	// instead of calling it multiple times in main()
+
+	
+	// Vectore for tiles used
+	std::vector<std::vector<int>> tilesUsed;
+
+	// Receive input
+	int input1 = 0;
+	int input2 = 0;
+
+	std::cout << "Please input row: ";
+	std::cin >> input1;
+	std::cout << "Please input column: ";
+	std::cin >> input2;
+
+	// Check against used tiles vector
+	std::vector<int> tempVector;
+	tempVector.push_back(input1);
+	tempVector.push_back(input2);
+
+	for (int i = 0; i < tempVector.size(); i++)
+	{
+		std::cout << tempVector[i];
+	}
+
+	// Compare input to used tiles
+
 }
